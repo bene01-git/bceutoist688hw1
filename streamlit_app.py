ac0a918f4e285d5e36c2a15f827f54cd31449560
Ben Euto <95237190+bene01-git@users.noreply.github.com>
2026-09-17 21:04:04 +0000

    Finished HW4

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,9 @@ st.title("IST688 HW Manager")
 
 page1 = st.Page('HW1.py', title="HW 1")
 page2 = st.Page('HW2.py', title="HW 2")
-page3 = st.Page('HW3.py', title="HW 3", default=True)
+page3 = st.Page('HW3.py', title="HW 3")
+page4 = st.Page('HW4.py', title="HW 4", default=True)
 
-pg = st.navigation([page1, page2, page3])
+pg = st.navigation([page1, page2, page3, page4])
 st.set_page_config(page_title="HWs")
 pg.run()
